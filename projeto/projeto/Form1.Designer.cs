b195a3ebbb9bdb5038e4093e64f14632a7195335
Bruno Shiohei <89222572+shioheii@users.noreply.github.com>
2023-04-02 11:47:52 -0300

    Add files via upload

---
+++ projeto/projeto/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 951);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 951);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(223, 951);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(329, 951);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(435, 951);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(541, 951);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(622, 954);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1042);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ projeto/projeto/Form1.cs
@@ -0,0 +1,94 @@
+﻿/* Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ * Curso Técnico em Informática - Data de Entrega: 22/03/2023
+ * Autores do Projeto: Arthur de Nazareth Falcão Braga
+ *                     Bruno Shiohei Kinoshita do Nascimento 
+ * Turma: 3H
+ * Atividade Proposta em aula
+ * Observação: <colocar se houver>
+ * 
+ * 
+ * ******************************************************************/
+
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int desenha = 0;
+        Color color(int r, int g, int b)
+        {
+            Color color = new Color();
+            color = Color.FromArgb(r, g, b);
+            return color;
+        }
+
+        void ponto(int x, int y, Color cor, PaintEventArgs e)
+        {
+            Pen caneta = new Pen(cor, 0);
+            e.Graphics.DrawLine(caneta, x, y, x + 1, y);
+        }
+
+        void linha(int xi, int yi, int xf, int yf, Pen caneta, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(caneta, xi, yi, xf, yf);
+        }
+
+        void desenharCoordenadas(PaintEventArgs e)
+        {
+            Color corEixosCartesianos = color(0, 0, 0);
+            Pen canetaEixosCartesianos = new Pen(corEixosCartesianos, 0);
+            linha(0, 540, 1920, 540, canetaEixosCartesianos, e);
+            linha(960, 0, 960, 1080, canetaEixosCartesianos, e);
+        }
+
+        void desenharLinha(PaintEventArgs e)
+        {
+            int xi = int.Parse(textBox1.Text);
+            int yi = int.Parse(textBox2.Text);
+            int m = int.Parse(textBox3.Text);
+            int xf = int.Parse(textBox4.Text);
+            int b = int.Parse(textBox5.Text);
+
+            int yf = 0;
+            label1.Text = "y = " + m + "x + " + b;
+
+            yi += b;
+            yf = m * xf + b;
+
+            Color corLinha = color(150, 0, 0);
+            Pen canetaLinha = new Pen(corLinha, 0);
+            linha(xi + 960, 540 - yi, xf + 960, 540 - yf, canetaLinha, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            desenha = 1;
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (desenha == 1)
+            {
+                desenharCoordenadas(e);
+                desenharLinha(e);
+            }
+        }
+    }
+}
